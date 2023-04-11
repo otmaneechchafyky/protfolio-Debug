822a9bef1de66aeb2114bbdbf15f46ab022638ec
Otmane Echchafyky (Dusk) <70438968+OTMANE-ECHCHAFYKY@users.noreply.github.com>
2023-04-11 10:23:40 +0000

    Create javascript file

---
+++ index.js
@@ -0,0 +1,8 @@
+const showHideMenu = document.querySelector(".menu-slider");
+function showHide() {
+  showHideMenu.classList.toggle("active");
+}
+const menuBtn = document.querySelector("#menuBtn");
+menuBtn.addEventListener("click", showHide);
+const xBtn = document.querySelector("#xBtn");
+xBtn.addEventListener("click", showHide);
