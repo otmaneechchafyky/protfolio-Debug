b27c33253dd0254cdc78feba7975197fa161979f
Otmane Echchafyky (Dusk) <70438968+OTMANE-ECHCHAFYKY@users.noreply.github.com>
2023-04-11 10:31:03 +0000

    delete js trials

--- index.js
+++
@@ -1,8 +0,0 @@
-const showHideMenu = document.querySelector(".menu-slider");
-function showHide() {
-  showHideMenu.classList.toggle("active");
-}
-const menuBtn = document.querySelector("#menuBtn");
-menuBtn.addEventListener("click", showHide);
-const xBtn = document.querySelector("#xBtn");
-xBtn.addEventListener("click", showHide);
