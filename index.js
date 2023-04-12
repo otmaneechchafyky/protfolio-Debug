938fdcfb29eddc55ccd4ad4b50e36107de213b8b
Otmane Echchafyky (Dusk) <70438968+OTMANE-ECHCHAFYKY@users.noreply.github.com>
2023-04-12 18:28:26 +0000

    Generate work section from js file

--- index.js
+++ index.js
@@ -6,3 +6,97 @@ const menuBtn = document.querySelector('#menuBtn');
 menuBtn.addEventListener('click', showHide);
 const xBtn = document.querySelector('#xBtn');
 xBtn.addEventListener('click', showHide);
+
+//popUp
+
+const projects = [
+  { 
+    name: "Data Dashboard Healthcare",
+    img: "./Assets/printing-work1.png",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    tags: ["HTML", "CSS", "Ruby"],
+    demoLink: "",
+    codeLink: "",
+  },
+  {
+    name: "Website Portfolio ",
+    img: "./Assets/printing-work1.png",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    tags: ["XML", "CSS", "Ruby"],
+    demoLink: "",
+    codeLink: "",
+  },
+  {
+    name: "Title 3",
+    img: "./Assets/gym.png",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    tags: ["HTML", "CSS", "Ruby"],
+    demoLink: "",
+    codeLink: "",
+  },
+  {
+    name: "Title 4",
+    img: "./Assets/gym.png",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    tags: ["HTML", "CSS", "Ruby"],
+    demoLink: "",
+    codeLink: "",
+  },
+  {
+    name: "Title 5",
+    img: "./Assets/gym.png",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    tags: ["HTML", "Bootstrap", "Ruby"],
+    demoLink: "",
+    codeLink: "",
+  },
+  {
+    name: "Title 6",
+    img: "./Assets/printing-work1.png",
+    description:
+      "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.",
+    tags: ["HTML", "CSS", "Javascript"],
+    demoLink: "",
+    codeLink: "",
+  },
+];
+console.log(projects);
+
+const works = document.getElementById('card-holder');
+
+for (let i = 0; i < projects.length; i++){
+
+  const cardHolder = document.createElement('div');
+  cardHolder.innerHTML = 
+  `
+  <div class="info-holder">
+    <h2 class="project-heading white-font">
+    ${ projects[i].name}
+    </h2>
+    <p class="card-description white-font">
+    ${ projects[i].description}
+    </p>
+    <ul class="tag-list white-font">
+
+      <li class="card-feature-items">${ projects[i].tags[0]}</li>
+      <li class="card-feature-items">${ projects[i].tags[1]}</li>
+      <li class="card-feature-items">${ projects[i].tags[2]}</li>
+    </ul>
+  </div>
+  <div class="button-section">
+    <a
+      id="cardButton${i}"
+      class="pop card-button dark-font seeInfo"
+      href="#popUp"
+      >See Project</a
+    >
+  </div>
+    `;
+    cardHolder.classList.add('work-card')
+  works.appendChild(cardHolder);
+}
